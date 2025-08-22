0bd8a8be77d8bf5e7f8f592aa97a3d08d08b4c7a
Kobe <kobe@wepunk.io>
2025-08-22 15:35:39 +0800

    6

--- main.js
+++ main.js
@@ -17,8 +17,7 @@ const messaging = getMessaging(app);
 
 // 注册 Service Worker
 if ('serviceWorker' in navigator) {
-    const basePath = window.location.pathname.replace(/\/$/, ''); // 去掉末尾斜杠
-    navigator.serviceWorker.register(`${basePath}/firebase-messaging-sw.js`, {scope: '/tg-test/'})
+    navigator.serviceWorker.register('/tg-test/firebase-messaging-sw.js')
         .then(reg => console.log('Service Worker 注册成功', reg))
         .catch(err => console.error('Service Worker 注册失败', err));
 }
